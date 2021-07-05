48c43eb4a8b8d0d1a62353280ef65ed85bf4b686
Bogdan <bandanovvs@gmail.com>
2021-07-05 23:49:33 +0300

    init

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import "materialize-css";
+import 'materialize-css/dist/css/materialize.min.css'
+
+const app = createApp(App)
+app.mount('#app')
