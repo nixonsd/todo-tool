1d7efea8be9e5050cf5365c9c6d4ab449d8430f2
Bogdan <bandanovvs@gmail.com>
2021-07-27 22:22:47 +0300

    Fix error

--- server/keys/keys.prod.js
+++ server/keys/keys.prod.js
@@ -2,7 +2,7 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   SESSION_SECRET: process.env.SESSION_SECRET,
   BASE_URL: process.env.BASE_URL,
-  PORT: process.env.PORT | 3000,
+  PORT: process.env.PORT,
   EMAIL_USER: process.env.EMAIL_USER,
   EMAIL_FROM: process.env.EMAIL_FROM,
   CLIENT_ID: process.env.CLIENT_ID,
