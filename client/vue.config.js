dd8e4f62f60efa24be223be564b6255cf1e65fd7
Bogdan <bandanovvs@gmail.com>
2021-07-30 18:47:16 +0300

    Deploy

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,17 +1,17 @@
 const path = require("path");
 
 const dotenv = require("dotenv");
-const dotenvExpand = require("dotenv-expand")
-var myEnv = dotenv.config({path: "../.env"});
+const dotenvExpand = require("dotenv-expand");
+var myEnv = dotenv.config({ path: "../.env" });
 dotenvExpand(myEnv);
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../server/public'),
+  outputDir: path.resolve(__dirname, "../server/public"),
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000'
-      }
-    }
+      "^/api": {
+        target: "http://localhost:3000/",
+      },
+    },
   },
 };
